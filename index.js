ccba049b6a7feb1cd354247388d14795ea1e28fe
theRingleman <sam.ringleman@gmail.com>
2016-09-12 13:48:06 -0600

    change the url and api request for random quote

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function getQuote(event) {
   $("#get-quote").on("click", function () {
-    alert("Hey you clicked me!");
-  })
+    var quote;
+    var randomQuoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&key";
+
+    $.getJSON(randomQuoteUrl, function (response) {
+      console.log(response);
+      $(".quote").append(response[0].content);
+    });
+  });
 }
 
 $(document).ready(function () {
