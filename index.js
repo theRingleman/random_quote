94c76b5ae9326a7d67291d076806f781c854a4c8
theRingleman <sam.ringleman@gmail.com>
2016-09-12 18:52:49 -0600

    add tweet url and a whole ton of other shit

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+var quote, author;
+
 function getQuote(event) {
   $("#get-quote").on("click", function () {
-    var quote;
-    var randomQuoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&key";
+    var randomQuoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
 
-    $.getJSON(randomQuoteUrl, function (response) {
-      console.log(response);
-      $(".quote").append(response[0].content);
-    });
+    $.get(randomQuoteUrl, function (response) {
+      quote = response.quoteText;
+      author = response.quoteAuthor;
+      appendQuote(quote, author);
+      setTweet(quote);
+    }, "jsonp");
   });
+}
+
+function appendQuote(quote, author) {
+  $(".quote").empty();
+  if (author === "") {
+    author = "Unknown";
+  }
+  $(".quote").append("<p>" + quote + " - " + author + "</p>");
+}
+
+function setTweet(quote) {
+  $("#tweet-link").attr("href", "http://twitter.com/home/?status=" + quote);
 }
 
 $(document).ready(function () {
